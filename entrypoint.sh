a643ada21e9e82ec722b8bea5f2c77cdf25f4eaa
DDSRem <1448139087@qq.com>
2022-08-18 13:03:56 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 
 first_lock="/bgmi_install.lock"
 bangumi_db="$BGMI_PATH/bangumi.db"
-transmission_setting="/bgmi/conf/transmission/settings.json"
 bgmi_nginx_conf="/bgmi/conf/nginx/bgmi.conf"
 bgmi_hardlink_helper="/bgmi/bgmi_hardlink_helper/bgmi_hardlink_helper.py"
 bgmi_hardlink_helper_config="/bgmi/bgmi_hardlink_helper/config.py"
@@ -25,7 +24,6 @@ function init_proc {
 		bgmi source $data_source
 		bgmi config ADMIN_TOKEN $admin_token
 		bgmi config SAVE_PATH /media/downloads
-		bgmi config DOWNLOAD_DELEGATE transmission-rpc
 	else
 		bgmi upgrade
 		bash /home/bgmi-docker/BGmi/bgmi/others/crontab.sh
@@ -34,30 +32,12 @@ function init_proc {
 	## 创建文件夹
 	mkdir -p /var/run/nginx
 	mkdir -p /bgmi/conf/bgmi
-	mkdir -p /bgmi/conf/transmission
 	mkdir -p /bgmi/conf/nginx
 	mkdir -p /bgmi/bgmi_hardlink_helper
 	mkdir -p /bgmi/log
 	mkdir -p /etc/supervisor.d
 	mkdir -p /media/cartoon
 	mkdir -p /media/downloads
-
-	## transmission
-	if [[ ${TRANSMISSION} = 'true' ]]; then
-		cp /home/bgmi-docker/config/bgmi_supervisord.ini /etc/supervisor.d/bgmi_supervisord.ini
-	else
-		cp /home/bgmi-docker/config/bgmi_supervisord-notransmission.ini /etc/supervisor.d/bgmi_supervisord.ini
-	fi
-
-	cp /home/bgmi-docker/config/transmission-daemon /etc/conf.d/transmission-daemon
-
-	if [ ! -f $transmission_setting ]; then
-		cp /home/bgmi-docker/config/transmission_settings.json $transmission_setting
-	fi
-
-	if [[ ${TRANSMISSION_WEB_CONTROL} = 'false' ]]; then
-		echo 3 | bash /home/bgmi-docker/transmission-web-control/install-tr-control-cn.sh > /dev/null
-	fi
 
 	## nginx
 	rm -rf /etc/nginx/conf.d
@@ -82,6 +62,15 @@ function init_proc {
 	## permission
 	groupmod -o -g "$PGID" abc
 	usermod -o -u "$PUID" abc
+
+	## downloader
+	if [[ ${DOWNLOADER} = 'transmission' ]]; then
+		bash /home/bgmi-docker/utils/transmission.sh
+	fi
+
+	if [[ ${DOWNLOADER} = 'aria2' ]]; then
+		bash /home/bgmi-docker/utils/aria2.sh
+	fi
 }
 
 if [ ! -f $first_lock ]; then
